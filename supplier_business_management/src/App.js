54b25018a280b5099424bb9f2da95feb7118cfc8
ananyachibber21 <ananyaunique21@gmail.com>
2024-11-24 22:50:37 +0530

    Created Dashboard Buttons

---
+++ supplier_business_management/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-main">
+        <button className="app-main-btn">Enquiry Addition Form</button>
+        <button className="app-main-btn">Client Addition Form</button>
+        <button className="app-main-btn">Add Company</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ supplier_business_management/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        'almond': '#EDE0D4',
+        'mustard': '#DDB892',
+        'light-brown': '#B08968',
+        'dark-brown': '#7F5539',
+      },
+    },
+  },
+  plugins: [],
+}
+
